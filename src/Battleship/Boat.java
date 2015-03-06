66be5c8b3b40c9494b46012dd97cda7e2effbdf4
Brian <brian.korduan@t-online>
2015-03-06 12:24:59 +0100

    Boat Klasse Abfrage ob zerstört

--- src/Battleship/Boat.java
+++ src/Battleship/Boat.java
@@ -4,15 +4,50 @@ import ch.aplu.jgamegrid.Location;
 
 public class Boat extends Ship {
 	
-	Location[] hitPoints = new Location[2];
 	
-	public Boat(Location a, Location b){
-		
-		hitPoints[0] = a;
 
+	Location[] position = new Location[2];
+	int size;
+	Boolean isDestroyed;
 
-	
-	
+	public Boat(int angle, int size, Location[] hitPoints) {
+		super(angle, size, hitPoints);
+
+		this.size = size;
+		for (Location hitPoint : hitPoints) {
+			int i = 0;
+			position[i] = hitPoint;
+			i++;
+		}
+	}
+
+	@Override
+	public Boolean isDestroyed(Location userShot) {
+
+		for (Location position : this.position) {
+
+			if (position.equals(userShot)) {
+				size--;
+			}
+			if (size == 0) {
+				isDestroyed = true;
+			} else {
+				isDestroyed = false;
+			}
+		}
+		return isDestroyed;
+	}
+
+	public void setLocation(Location a, Location b) {
+
+	}
+
+	public Location[] getHitPoints() {
+		return hitPoints;
+	}
+
+	public void setHitPoints(Location[] hitPoints) {
+		this.hitPoints = hitPoints;
 	}
 
 }
