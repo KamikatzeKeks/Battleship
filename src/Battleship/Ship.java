0ff27e257f21dbd9ffcd86765006540bbbcbd7a3
Brian <brian.korduan@t-online>
2015-03-06 12:33:11 +0100

    Ship

--- src/Battleship/Ship.java
+++ src/Battleship/Ship.java
@@ -4,13 +4,27 @@ import java.awt.image.BufferedImage;
 
 import ch.aplu.jgamegrid.*;
 
-public class Ship extends Actor
+public abstract class Ship extends Actor
 {
 	private int angle;
+	private int size;
+	private Boolean isDestroyed = false; 
 
-	public Ship(int angle) 
-	{		
+	public Ship(int angle, int size, Location[] hitPoints) 
+ 	{		
 		super("sprites/destroyer_" + angle + ".gif", 1);
 		this.angle = angle;
 	}
+	
+	
+	
+	 public Ship(int angle2, int size2, Location[] hitPoints) {
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public abstract  Boolean  isDestroyed(Location userShot);
+	
+
 }
