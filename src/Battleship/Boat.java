9838fc7171cdb106aff90d7b22fe2afa3fa7e3d9
Brian <brian.korduan@t-online>
2015-03-06 08:26:43 +0100

    Erste Version

---
+++ src/Battleship/Boat.java
@@ -0,0 +1,18 @@
+package Battleship;
+
+import ch.aplu.jgamegrid.Location;
+
+public class Boat extends Ship {
+	
+	Location[] hitPoints = new Location[2];
+	
+	public Boat(Location a, Location b){
+		
+		hitPoints[0] = a;
+
+
+	
+	
+	}
+
+}
---
+++ src/Battleship/Ship.java
@@ -0,0 +1,17 @@
+package Battleship;
+
+import ch.aplu.jgamegrid.*;
+
+public abstract class Ship extends Actor
+{
+
+	public Ship() 
+	{
+		super("src/sprites/destroyer.gif", 2);
+	}
+	
+	public Ship( Location x){
+		
+	}
+
+}
