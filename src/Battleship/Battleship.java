ab05add7b876fa63022f66de375ddc8c4ccc52b2
Brian <brian.korduan@t-online>
2015-03-10 10:29:00 +0100

    Noch mehr aufgeräumt

--- src/Battleship/Battleship.java
+++ src/Battleship/Battleship.java
@@ -7,116 +7,114 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+public class Battleship extends JFrame {
+	GameGrid jGameGridPlayer1 = new GameGrid(10, 10, 60, Color.green,
+			"sprites/background.png");
+	GameGrid jGameGridPlayer2 = new GameGrid(10, 10, 60, Color.red,
+			"sprites/background.png");
 
-public class Battleship extends JFrame
-{
-	GameGrid jGameGridPlayer1 = new GameGrid(10, 10, 60, Color.green, "sprites/background.png");
-	GameGrid jGameGridPlayer2 = new GameGrid(10, 10, 60, Color.red, "sprites/background.png");
-	
-	JButton jBtnSetFleetPlayer1 = new JButton("Set Fleet Player 1"); 
+	JButton jBtnSetFleetPlayer1 = new JButton("Set Fleet Player 1");
 
-	Ship[] fleetPlayer1 = new Ship[5]; 
+	Ship[] fleetPlayer1 = new Ship[5];
 	Ship[] fleetPlayer2 = new Ship[5];
-	
-	
-	
-	
-	public Battleship()
-	{
-		//Sets values of Mainframe
+
+	public Battleship() {
+		// Sets values of Mainframe
 		{
-		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-	    this.setSize(1350,700);
+			this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+			this.setSize(1350, 700);
 		}
-		
+
 		{
-		    jBtnSetFleetPlayer1.setSize(new Dimension(100,50));
-		    getContentPane().add(jBtnSetFleetPlayer1, BorderLayout.SOUTH);
+			jBtnSetFleetPlayer1.setSize(new Dimension(100, 50));
+			getContentPane().add(jBtnSetFleetPlayer1, BorderLayout.SOUTH);
 		}
-		
-		//GameGrid Player1
+
+		// GameGrid Player1
 		{
-		getContentPane().add(jGameGridPlayer1, BorderLayout.WEST);
-	    jGameGridPlayer1.doRun();
+			getContentPane().add(jGameGridPlayer1, BorderLayout.WEST);
+			jGameGridPlayer1.doRun();
 		}
-		
-		//GameGrid Player2
+
+		// GameGrid Player2
 		{
-	    getContentPane().add(jGameGridPlayer2, BorderLayout.EAST);
-	    jGameGridPlayer2.doRun();
+			getContentPane().add(jGameGridPlayer2, BorderLayout.EAST);
+			jGameGridPlayer2.doRun();
 		}
-		
+
 		// Action Listeners
 		{
-	    jBtnSetFleetPlayer1.addActionListener(new ActionListener() 
-	    {
-	         public void actionPerformed(ActionEvent e)
-	         {
-	        	 
-	        	 FleetSetterFrame player1 = new FleetSetterFrame(null, true); // Erstellt modalen jDialog
-	        	  fleetPlayer1 = player1.showDialog();  //Zeigt den Dialog und bekommt das Array aus Shiffobjekten vom Dialog
-	        	  //TODO Shiffobjekte im Dialog erzeugen und im return Array speichern <<< mit der public void Game() Methode;
-	        	//Game();
-	         }          
-	    });
+			jBtnSetFleetPlayer1.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) // Aktion beimDrücken desButtons "Set Fleet Player 1"													
+				{
+
+					FleetSetterFrame player1 = new FleetSetterFrame(null, true); // Erstellt modalen  jDialog
+					fleetPlayer1 = player1.showDialog(); // Zeigt den Dialog und bekommt das Array aus Shiffobjekten  vom Dialog
+					
+					// TODO Shiffobjekte im Dialog erzeugen und im return Array
+					// speichern <<< mit der public void Game() Methode;
+					
+					// Game();
+				}
+			});
 		}
-	    
-	}
-	
-//	public void Game()
-//	{
-//		Location[] position = new Location[2];
-//		
-//		for(int i=0; i<5; i++ )
-//		{
-//			
-//			int x = Integer.parseInt(JOptionPane.showInputDialog("Bitte X Koordinat angeben"));
-//	 		int y = Integer.parseInt(JOptionPane.showInputDialog("Bitte Y Koordinat angeben"));
-//	 		int angle = Integer.parseInt(JOptionPane.showInputDialog("Bitte Winkel angeben (0,90, 180, 270)"));
-//	 		
-//	 		Location l = new Location(x-1, y-1);
-//	 		position[0] = l;
-//	 		
-//	 		
-//	 		Location l1 = new Location();
-//	 		switch (angle) 
-//	 		{
-//	            case 0:  l1 = new Location(x, y-1);
-//	                     break;
-//	            case 90: l1 = new Location(x-1, y);
-//	                     break;
-//	            case 180:  l1 = new Location(x-2, y-1);
-//	                     break;
-//	            case 270:  l1 = new Location(x-1, y-2);
-//	                     break;
-//	            default: l1 = new Location(x, y-1);
-//	                     break;
-//	 		}
-//	 		position[0] = l;
-//	 		position[1] = l1;
-//	 		
-//	 		Boat b = new Boat(angle, 3, position);
-//	 		fleet[i] = b;
-//	 	    gg.addActor(b, l);
-//		}
-//		b.setVisible(false);
-//	}
-	
-	private void addShipToFleet(){
-		
+
 	}
 
-	public static void main(String[] args) 
-	{
-		EventQueue.invokeLater(new Runnable()
-	    {
-	      public void run()
-	      {
-	    	  new Battleship().setVisible(true);
-	    	  
-	      }
-	    });
-		
+	// public void Game()
+	// {
+	// Location[] position = new Location[2];
+	//
+	// for(int i=0; i<5; i++ )
+	// {
+	//
+	// int x =
+	// Integer.parseInt(JOptionPane.showInputDialog("Bitte X Koordinat angeben"));
+	// int y =
+	// Integer.parseInt(JOptionPane.showInputDialog("Bitte Y Koordinat angeben"));
+	// int angle =
+	// Integer.parseInt(JOptionPane.showInputDialog("Bitte Winkel angeben (0,90, 180, 270)"));
+	//
+	// Location l = new Location(x-1, y-1);
+	// position[0] = l;
+	//
+	//
+	// Location l1 = new Location();
+	// switch (angle)
+	// {
+	// case 0: l1 = new Location(x, y-1);
+	// break;
+	// case 90: l1 = new Location(x-1, y);
+	// break;
+	// case 180: l1 = new Location(x-2, y-1);
+	// break;
+	// case 270: l1 = new Location(x-1, y-2);
+	// break;
+	// default: l1 = new Location(x, y-1);
+	// break;
+	// }
+	// position[0] = l;
+	// position[1] = l1;
+	//
+	// Boat b = new Boat(angle, 3, position);
+	// fleet[i] = b;
+	// gg.addActor(b, l);
+	// }
+	// b.setVisible(false);
+	// }
+
+	private void addShipToFleet() {
+
+	}
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new Battleship().setVisible(true);
+
+			}
+		});
+
 	}
 
 }
