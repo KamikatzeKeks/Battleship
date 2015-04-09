c513ec56b6d4ff6ab29361eb8bdc861d046ba9ed
Brian <brian.korduan@t-online>
2015-04-09 15:52:27 +0200

    Dokumentation

--- src/Battleship/Ship.java
+++ src/Battleship/Ship.java
@@ -2,8 +2,28 @@ package Battleship;
 
 import java.awt.image.BufferedImage;
 
+
+
 import ch.aplu.jgamegrid.*;
 
+/**
+ * Die abstrakte Klasse Ship erbt von Actor und repräsentiert ein Schiff auf dem Spielfeld.
+ * Jedes Objekt der Klasse Schiff besteht aus einem Integer der die Größe in Spielfeldern angibt,
+ * einem int der den Winkel angibt in welchen das Schiff platziert wird, einem Boolean der angibt ob ein Schiff zerstört
+ * wurde oder nicht
+ * 
+ * 
+ * Jedes Schiff besteht vorallem aus einem Array von Objekten der Klasse Location. 
+ * Ein Schiff der Größe " 5 " besteht z.B. aus 5 Objekten der Klasse Location, da es 5 Spielfelder belegt.
+ * 
+ * @param angle
+ * @param size 
+ * @param hitPoints
+ * @param shipType
+ * 
+ * @author Brian Korduan
+ * @author Davis Fröse
+ */
 public abstract class Ship extends Actor {
 	private int angle;
 	private int size;
@@ -17,7 +37,11 @@ public abstract class Ship extends Actor {
 		this.angle = angle;
 		this.size = size;
 	}
-	
+	/**
+	 * 
+	 * @param userShot
+	 * @return
+	 */
 	public int isHit(Location userShot)
 	{
 		int isHitOrDestroyed = 0; // 0=nicht getroffen, 1=getroffen, 2= getroffen und versenkt
@@ -39,12 +63,18 @@ public abstract class Ship extends Actor {
 		return isHitOrDestroyed;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public Location getStartLocation(){
 				
 		return hitPoints[hitPoints.length -1];
 		
 	}
-
+/**
+ * 
+ */
 	public void showLocationPosition() {
 
 		for (Location bla : hitPoints) {
@@ -54,6 +84,11 @@ public abstract class Ship extends Actor {
 		}
 
 	}
+	
+	/**
+	 * 
+	 * @return
+	 */
 	
 	public Location[] getShipPositions(){
 		 return hitPoints;
